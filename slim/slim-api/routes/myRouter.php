191783318820006cd8cedd51d06b133bf395f592
bengugultekin <bengunurgultekin54@gmail.com>
2022-08-17 16:06:19 +0300

    editted post method

--- slim/slim-api/routes/myRouter.php
+++ slim/slim-api/routes/myRouter.php
@@ -37,7 +37,7 @@ $app->get('/machines/all', function (Request $request, Response $response) {
     }
 });
 
-$app->get('/machines/{id}', function (Request $request, Response $response, array $args) {
+$app->get('/machines/all/{id}', function (Request $request, Response $response, array $args) {
     $id = $args['id'];
     $sql = "SELECT * FROM containers WHERE container_id=$id";
     try {
@@ -95,35 +95,39 @@ $app->get('/machines/del/{id}', function (Request $request, Response $response, 
     }
 });
 
-// Düzeltilecek
-$app->post('machines/new/', function(Request $request, Response $response, array $args) {
-    $name = $args['containername'];
-    $date = time();    
-    $sql= "INSERT INTO containers WHERE (containername) VALUES ('$name')";
+// Time type typeDüzeltilecek
+$app->post('/machines/add', function (Request $request, Response $response, array $args) {
+    $data = $request->getParsedBody();
+    $containername = $data["containername"];
+    $date = date_timestamp_get(date_create());
+   
+    $sql = "INSERT INTO containers (containername, created_on) VALUES (:containername, :created_on)";
+   
     try {
-        $db = new DB();
-        $conn = $db->connect();
-
-        $stmt = $conn->query($sql);
-        $machines = $stmt->fetchAll(PDO::FETCH_OBJ);
-
-        // Make db null so that we do not get error when we do another db request
-        $db = null;
-        
-        $response->getBody()->write(json_encode($machines));
-        return $response
-            ->withHeader("content-type", "application/json")
-            ->withStatus(200);
+      $db = new Db();
+      $conn = $db->connect();
+     
+      $stmt = $conn->prepare($sql);
+      $stmt->bindParam(':containername', $containername);
+      $stmt->bindParam(':created_on', $date);
+   
+      $result = $stmt->execute();
+   
+      $db = null;
+      $response->getBody()->write(json_encode($result));
+      return $response
+        ->withHeader('content-type', 'application/json')
+        ->withStatus(200);
+    } catch (PDOException $e) {
+      $error = array(
+        "message" => $e->getMessage()
+      );
+   
+      $response->getBody()->write(json_encode($error));
+      return $response
+        ->withHeader('content-type', 'application/json')
+        ->withStatus(500);
     }
-    catch (PDOException $e) {
-        $error = array(
-            "message" => $e->getMessage()
-        );
-        $response->getBody()->write(json_encode($error));
-        return $response
-            ->withHeader("content-type", "application/json")
-            ->withStatus(500);
-    }
-});
+   });
 
 ?>
