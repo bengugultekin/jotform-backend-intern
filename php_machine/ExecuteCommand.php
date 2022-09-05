4bd3d0d47ed18aafae992b4e525df2c8f9f92bf6
gookhan <tasgokhan389@hotmail.com>
2022-09-05 12:50:17 +0300

    added invoking php script with crontab file

--- php_machine/ExecuteCommand.php
+++ php_machine/ExecuteCommand.php
@@ -1,3 +1,20 @@
 <?php
-echo "Hello world\n";
+$url = "http://server_machine:80/v1/machine/1/exec";
+#$url = "http://localhost:9000/v1/machine/1/exec";
+
+$data = array('command' => 'pwd');
+
+$options = array(
+    'http' => array(
+      'method'  => 'POST',
+      'content' => json_encode( $data ),
+      'header'=>  "Content-Type: application/json\r\n" .
+                  "Accept: application/json\r\n"
+      )
+  );
+  
+  $context  = stream_context_create( $options );
+  $result = file_get_contents( $url, false, $context );
+  $response = json_decode( $result );
+
 ?>
